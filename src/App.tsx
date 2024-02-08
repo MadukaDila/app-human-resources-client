8ef0ed1ba11fc5584797ac5feadf58f6319658b8
Amila T Kumarasekara <42783431+MadukaDila@users.noreply.github.com>
2024-02-08 14:01:58 +0530

    import and configure the routes of the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
+import Homepage from "./pages/home";
+import AttendanceUpload from "./pages/upload";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/upload" element={<AttendanceUpload />} />
+        </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
